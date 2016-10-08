a44669da612d6dda61d4e1942750e1baa5e2cffd
Mert Kaplan <mertkaplan.tr@gmail.com>
2016-10-08 17:48:52 +0300

    first

---
+++ Linux Kernel/USB PlugIn/ReadMe.txt
@@ -0,0 +1,1 @@
+Copy 10-mert-kaplan.rules file to /etc/udev/rules.d with root permissions.
---
+++ Linux Kernel/USB PlugIn/autorun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# This is a comment!
+echo "This is test script" > /home/debian/TestScriptGenerated.txt
